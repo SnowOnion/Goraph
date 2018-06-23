8db951e252fd30278bb5fe3aac0dc665914373bf
SnowOnion <snowonionlee@gmail.com>
2018-06-23 20:05:04 +0800

    Found bug while Refactoring :( still buggy

--- script/index.js
+++ script/index.js
@@ -1,12 +1,50 @@
-// https://stackoverflow.com/a/18053642/2801663
-function getCursorPosition(canvas, event) {
-    let rect = canvas.getBoundingClientRect();
-    let x = event.clientX - rect.left;
-    let y = event.clientY - rect.top;
-    return [x, y];
-}
+/**
+ * 拓扑与坐标无关……
+ * */
+const adj0 = {
+    0: [1, 3],
+    1: [0, 2, 3, 4],
+    2: [1, 4],
+    3: [0, 1, 5, 6],
+    4: [1, 2, 6, 7],
+    5: [3, 6],
+    6: [3, 4, 5, 7],
+    7: [4, 6]
+};
 
-let global = {
+/**
+ * 游戏状态又与棋盘形状无关……
+ */
+const positionedNodes = {
+    0: {x: 250, y: 250},
+    1: {x: 500, y: 250},
+    2: {x: 750, y: 250},
+    3: {x: 250, y: 500},
+    4: {x: 750, y: 500},
+    5: {x: 250, y: 750},
+    6: {x: 500, y: 750},
+    7: {x: 750, y: 750}
+};
+
+let global, gameStatus, stoneStrings;
+
+/**
+ * stone string id -> {chiSet: Set<node id>; nodes: Set<node id>}
+ * Damn JS type system
+ * @type {{}}
+ */
+stoneStrings = {};
+gameStatus = {
+    0: {s: 0, stoneStringID: null},
+    1: {s: 0, stoneStringID: null},
+    2: {s: 0, stoneStringID: null},
+    3: {s: 0, stoneStringID: null},
+    4: {s: 0, stoneStringID: null},
+    5: {s: 0, stoneStringID: null},
+    6: {s: 0, stoneStringID: null},
+    7: {s: 0, stoneStringID: null}
+};
+global = {
     next: 1,
     oppoColor: function (myColor) {
         return 3 - myColor;
@@ -21,29 +59,13 @@ let global = {
     },
     _nextStoneStringID: 0,
     nextStoneStringID: function () {
-        return global._nextStoneStringID++;
+        return this._nextStoneStringID++;
     }
 };
 
-/**
- * stone string id -> {chiSet: Set<node id>; nodes: Set<node id>}
- * Damn JS type system
- * @type {{}}
- */
-let stoneStrings = {};
-
-let gameStatus = {
-    0: {s: 0, stoneStringID: null},
-    1: {s: 0, stoneStringID: null},
-    2: {s: 0, stoneStringID: null},
-    3: {s: 0, stoneStringID: null},
-    4: {s: 0, stoneStringID: null},
-    5: {s: 0, stoneStringID: null},
-    6: {s: 0, stoneStringID: null},
-    7: {s: 0, stoneStringID: null}
-};
 
 function init() {
+    initData();
     drawAccordingToStatus();
 
     let canvas = document.getElementById('canvas');
@@ -57,7 +79,6 @@ function init() {
 
         // n for both nearest / node
         let nid = findNearestNodeID(xn, yn);
-        // console.log("before update", nid, gameStatus, stoneStrings);
         if (notOccupied(nid) && validWrtRuleAndUpdate(nid, global.next)) {
             global.flip();
             drawAccordingToStatus();
@@ -68,55 +89,177 @@ function init() {
         }
 
     });
+
+    function initData() {
+
+    }
+
+    // https://stackoverflow.com/a/18053642/2801663
+    function getCursorPosition(canvas, event) {
+        let rect = canvas.getBoundingClientRect();
+        let x = event.clientX - rect.left;
+        let y = event.clientY - rect.top;
+        return [x, y];
+    }
+
+
+    function findNearestNodeID(xn, yn) {
+        let dSquare = Number.MAX_VALUE,
+            result;
+        for (let id in positionedNodes) {
+            let pNode = positionedNodes[id];
+            let x = pNode.x,
+                y = pNode.y,
+                newDSquare = (x - xn) * (x - xn) + (y - yn) * (y - yn);
+            if (newDSquare < dSquare) {
+                dSquare = newDSquare;
+                result = id;
+            }
+        }
+        return parseInt(result); // not a big deal?
+    }
+}
+
+function drawAccordingToStatus() {
+    let canvas = document.getElementById('canvas');
+    let ctx = canvas.getContext('2d');
+    if (canvas.getContext) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawBoard(ctx);
+        for (let id in gameStatus) {
+            if (gameStatus[id].s !== 0) {
+                let x = positionedNodes[id].x,
+                    y = positionedNodes[id].y,
+                    color = gameStatus[id].s;
+                drawStone(x, y, color);
+            }
+        }
+    }
+
+    /**
+     * 结点画个小黑圆点；
+     * 只从小 id 向大 id 画线，避免画两遍；
+     */
+    function drawBoard(ctx) {
+        for (let id1 in positionedNodes) {
+            let x1 = positionedNodes[id1].x,
+                y1 = positionedNodes[id1].y;
+            drawLittleBlack(x1, y1);
+
+            for (let id2 of adj0[id1]) {
+                if (id1 <= id2) {
+                    let x2 = positionedNodes[id2].x,
+                        y2 = positionedNodes[id2].y;
+                    drawLine(x1, y1, x2, y2);
+                }
+            }
+        }
+
+        /**
+         * 嵌套定义 function 提升出去什么的……看看 MetaTODO
+         */
+        function drawLine(x1, y1, x2, y2) {
+            ctx.fillStyle = "black";
+            ctx.beginPath();
+            ctx.moveTo(x1, y1);
+            ctx.lineTo(x2, y2);
+            ctx.stroke(); // 最后一起 stroke 以提升性能？……
+        }
+
+        function drawLittleBlack(x, y) {
+            ctx.fillStyle = "black";
+            ctx.beginPath();
+            ctx.arc(x, y, 3, 0, Math.PI * 2);
+            ctx.fill();
+        }
+    }
+
+
+    /**
+     * @param x
+     * @param y
+     * @param colorEnum 1 black, 2 white 无防御
+     */
+    function drawStone(x, y, colorEnum) {
+        ctx.fillStyle = colorEnum === 1 ? "black" : "white";
+        ctx.beginPath();
+        ctx.arc(x, y, 35, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.fillStyle = "black";
+        ctx.arc(x, y, 35, 0, Math.PI * 2);
+        ctx.stroke();
+    }
 }
 
 /**
- * 游戏状态又与棋盘形状无关……
+ *
+ * @param msg No argument = clear
  */
-let positionedNodes = {
-    0: {x: 250, y: 250},
-    1: {x: 500, y: 250},
-    2: {x: 750, y: 250},
-    3: {x: 250, y: 500},
-    4: {x: 750, y: 500},
-    5: {x: 250, y: 750},
-    6: {x: 500, y: 750},
-    7: {x: 750, y: 750}
-};
-
-function findNearestNodeID(xn, yn) {
-    let dSquare = Number.MAX_VALUE,
-        result;
-    for (let id in positionedNodes) {
-        let pNode = positionedNodes[id];
-        let x = pNode.x,
-            y = pNode.y,
-            newDSquare = (x - xn) * (x - xn) + (y - yn) * (y - yn);
-        if (newDSquare < dSquare) {
-            dSquare = newDSquare;
-            result = id;
-        }
-    }
-    return parseInt(result); // not a big deal
+function infoLog(msg) {
+    msg = msg || "　";
+    let info = document.getElementById('info');
+    info.innerHTML = msg;
 }
+
+function restart() {
+
+}
+
+function pass() {
+    global.flip();
+    drawAccordingToStatus();
+    infoLog();
+}
+
+
+
 
 function notOccupied(nodeID) {
     return gameStatus[nodeID].s === 0;
 }
 
 /**
- * 拓扑与坐标无关……
+ * If this move is valid, update data, else keep the data as is and return null
+ *
+ * Interface for different rules (Chinese, Japanese,...)
+ *
+ * 中国规则：禁止全盘同形再现（蕴含了打劫规则？）TODO
+ * 日本规则：TODO
+ *
+ * @param nodeID 此次落子处
+ * @param color 1 black 2 white
+ *
  * */
-let adj0 = {
-    0: [1, 3],
-    1: [0, 2, 3, 4],
-    2: [1, 4],
-    3: [0, 1, 5, 6],
-    4: [1, 2, 6, 7],
-    5: [3, 6],
-    6: [3, 4, 5, 7],
-    7: [4, 6]
-};
+function validWrtRuleAndUpdate(nodeID, color) {
+    let neighborMySSs, neighborOppoSSs, // Set<Int>
+        directChi; // Int
+    [neighborMySSs, neighborOppoSSs, directChi] = findNeighborSSnChi(nodeID, color);
+
+    let existsEnemyToRemove = neighborOppoSSs.some(function (ssid) {
+        return stoneStrings[ssid].chiSet.size === 1;
+    });
+
+    let existsFriendChiMoreThanTwo = neighborMySSs.some(function (ssid) {
+        return stoneStrings[ssid].chiSet.size >= 2;
+    });
+
+    let validMove = (directChi > 0) || existsEnemyToRemove || existsFriendChiMoreThanTwo;
+    if (validMove) {
+        // 既然不会回滚，那么可以先连自己（或新建棋串），再提敌子儿。提敌子儿时也会更新我方气集的。
+        // 相连之后的新气集：旧棋串气集、新落子气集的（可交）union - {nodeID}
+        // 相连之后的新子儿集：旧棋串子儿集的（不交）union ∪ {nodeID}
+        buildOrMergeSS(nodeID, color, neighborMySSs);
+
+        // 给敌方紧气，maybe 提子儿
+        affectOpponents(nodeID, color, neighborOppoSSs);
+
+        return true;
+    }
+    else {
+        return null;
+    }
+}
+
 
 function findNeighborSSnChi(nodeID, color) {
     // 先找出所有 distinct 邻居棋串（避免对同一棋串重复操作），按敌我分开；也同时算出直接的气数
@@ -137,6 +280,48 @@ function findNeighborSSnChi(nodeID, color) {
         }
     }
     return [[...neighborMySSs], [...neighborOppoSSs], directChi];
+}
+
+function buildOrMergeSS(nodeID, color, neighborMySSs) {
+    // 两种情形都要做的事情
+    let newStoneChiSet = calcNewStoneChiSet(nodeID);
+    let newSSID = global.nextStoneStringID();
+    gameStatus[nodeID].s = color;
+    gameStatus[nodeID].stoneStringID = newSSID;
+
+    // 无我方邻居棋串，则新建棋串
+    if (neighborMySSs.length === 0) {
+        stoneStrings[newSSID] = {chiSet: newStoneChiSet, nodes: new Set([nodeID])};
+    } else {// 有我方邻居棋串，则合并相邻的我方 n(>0) 个棋串
+        let newNodeSet = new Set([nodeID]), newChiSet = new Set(newStoneChiSet);
+        for (let ssid of neighborMySSs) {
+            stoneStrings[ssid].nodes.forEach(function (nidOfExistingFriend) {
+                // Last (hope so) graph algorithm bug here: `nidOfExistingFriend` was `ele` then, and I did `gameStatus[ele].s = color;`. 成了给黑子涂黑色。
+                newNodeSet.add(nidOfExistingFriend);
+                gameStatus[nidOfExistingFriend].stoneStringID = newSSID;
+            });
+            stoneStrings[ssid].chiSet.forEach(function (chiEle) {
+                newChiSet.add(chiEle)
+            });
+            delete stoneStrings[ssid];
+        }
+        newChiSet.delete(nodeID);
+        // newNodeSet, newChiSet OK
+
+        stoneStrings[newSSID] = {chiSet: newChiSet, nodes: newNodeSet};
+    }
+
+    /**
+     * 在提掉敌子前，计算新落子自己的气集合
+     * @param nodeID
+     * @returns {Set}
+     */
+    function calcNewStoneChiSet(nodeID) {
+        return new Set(adj0[nodeID].filter(function (nid) {
+            let neighborColor = gameStatus[nid].s;
+            return global.isEmpty(neighborColor);
+        }));
+    }
 }
 
 /**
@@ -188,169 +373,4 @@ function removeStoneString(removedNID, ssidToRemove, colorToRemove) {
         });
 
     });
-}
-
-/**
- * 在提掉敌子前，计算新落子自己的气集合
- * @param nodeID
- * @returns {Set}
- */
-function calcNewStoneChiSet(nodeID) {
-    return new Set(adj0[nodeID].filter(function (nid) {
-        let neighborColor = gameStatus[nid].s;
-        return global.isEmpty(neighborColor);
-    }));
-}
-
-function buildOrMergeSS(nodeID, color, neighborMySSs) {
-    // 两种情形都要做的事情
-    let newStoneChiSet = calcNewStoneChiSet(nodeID);
-    let newSSID = global.nextStoneStringID();
-    gameStatus[nodeID].s = color;
-    gameStatus[nodeID].stoneStringID = newSSID;
-
-    // 无我方邻居棋串，则新建棋串
-    if (neighborMySSs.length === 0) {
-        stoneStrings[newSSID] = {chiSet: newStoneChiSet, nodes: new Set([nodeID])};
-    } else {// 有我方邻居棋串，则合并相邻的我方 n(>0) 个棋串
-        let newNodeSet = new Set([nodeID]), newChiSet = new Set(newStoneChiSet);
-        for (let ssid of neighborMySSs) {
-            stoneStrings[ssid].nodes.forEach(function (nidOfExistingFriend) {
-                // Last (hope so) graph algorithm bug here: `nidOfExistingFriend` was `ele` then, and I did `gameStatus[ele].s = color;`. 成了给黑子涂黑色。
-                newNodeSet.add(nidOfExistingFriend);
-                gameStatus[nidOfExistingFriend].stoneStringID = newSSID;
-            });
-            stoneStrings[ssid].chiSet.forEach(function (chiEle) {
-                newChiSet.add(chiEle)
-            });
-            delete stoneStrings[ssid];
-        }
-        newChiSet.delete(nodeID);
-        // newNodeSet, newChiSet OK
-
-        stoneStrings[newSSID] = {chiSet: newChiSet, nodes: newNodeSet};
-    }
-}
-
-/**
- * If this move is valid, update data, else keep the data as is and return null
- *
- * Interface for different rules (Chinese, Japanese,...)
- *
- * 中国规则：禁止全盘同形再现（蕴含了打劫规则？）TODO
- * 日本规则：TODO
- *
- * @param nodeID 此次落子处
- * @param color 1 black 2 white
- *
- * */
-function validWrtRuleAndUpdate(nodeID, color) {
-    let neighborMySSs, neighborOppoSSs, // Set<Int>
-        directChi; // Int
-    [neighborMySSs, neighborOppoSSs, directChi] = findNeighborSSnChi(nodeID, color);
-
-    let existsEnemyToRemove = neighborOppoSSs.some(function (ssid) {
-        return stoneStrings[ssid].chiSet.size === 1;
-    });
-
-    let existsFriendChiMoreThanTwo = neighborMySSs.some(function (ssid) {
-        return stoneStrings[ssid].chiSet.size >= 2;
-    });
-
-    let validMove = (directChi > 0) || existsEnemyToRemove || existsFriendChiMoreThanTwo;
-    if (validMove) {
-        // 既然不会回滚，那么可以先连自己（或新建棋串），再提敌子儿。提敌子儿时也会更新我方气集的。
-        // 相连之后的新气集：旧棋串气集、新落子气集的（可交）union - {nodeID}
-        // 相连之后的新子儿集：旧棋串子儿集的（不交）union ∪ {nodeID}
-        buildOrMergeSS(nodeID, color, neighborMySSs);
-
-        // 给敌方紧气，maybe 提子儿
-        affectOpponents(nodeID, color, neighborOppoSSs);
-
-        return true;
-    }
-    else {
-        return null;
-    }
-}
-
-
-/**
- * 结点画个小黑圆点；
- * 只从小 id 向大 id 画线，避免画两遍；
- */
-function drawBoard(ctx) {
-    for (let id1 in positionedNodes) {
-        let x1 = positionedNodes[id1].x,
-            y1 = positionedNodes[id1].y;
-        drawLittleBlack(x1, y1);
-
-        for (let id2 of adj0[id1]) {
-            if (id1 <= id2) {
-                let x2 = positionedNodes[id2].x,
-                    y2 = positionedNodes[id2].y;
-                drawLine(x1, y1, x2, y2);
-            }
-        }
-    }
-
-    /**
-     * 嵌套定义 function 提升出去什么的……看看 MetaTODO
-     */
-    function drawLine(x1, y1, x2, y2) {
-        ctx.fillStyle = "black";
-        ctx.beginPath();
-        ctx.moveTo(x1, y1);
-        ctx.lineTo(x2, y2);
-        ctx.stroke(); // 最后一起 stroke 以提升性能？……
-    }
-
-    function drawLittleBlack(x, y) {
-        ctx.fillStyle = "black";
-        ctx.beginPath();
-        ctx.arc(x, y, 3, 0, Math.PI * 2);
-        ctx.fill();
-    }
-}
-
-function drawAccordingToStatus() {
-    let canvas = document.getElementById('canvas');
-    let ctx = canvas.getContext('2d');
-    if (canvas.getContext) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        drawBoard(ctx);
-        for (let id in gameStatus) {
-            if (gameStatus[id].s !== 0) {
-                let x = positionedNodes[id].x,
-                    y = positionedNodes[id].y,
-                    color = gameStatus[id].s;
-                drawStone(x, y, color);
-            }
-        }
-    }
-
-    /**
-     * @param x
-     * @param y
-     * @param colorEnum 1 black, 2 white 无防御
-     */
-    function drawStone(x, y, colorEnum) {
-        ctx.fillStyle = colorEnum === 1 ? "black" : "white";
-        ctx.beginPath();
-        ctx.arc(x, y, 25, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.fillStyle = "black";
-        ctx.arc(x, y, 25, 0, Math.PI * 2);
-        ctx.stroke();
-    }
-}
-
-/**
- *
- * @param msg No argument = clear
- */
-function infoLog(msg) {
-    msg = msg || "　";
-    let info = document.getElementById('info');
-    info.innerHTML = msg;
 }
